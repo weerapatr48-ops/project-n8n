a910a571750299e62054abf6894eb258bbe7196c
weerapatr48-ops <weerapat.r48@gmail.com>
2026-07-15 18:15:03 +0700

    undo: remove localtunnel auto-bypass code

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Intercept all fetch requests to automatically bypass localtunnel warning
-const originalFetch = window.fetch;
-window.fetch = async (...args) => {
-  let [resource, config] = args;
-  if (!config) config = {};
-  if (!config.headers) config.headers = {};
-  
-  if (config.headers instanceof Headers) {
-    config.headers.set('bypass-tunnel-reminder', 'true');
-  } else {
-    config.headers['bypass-tunnel-reminder'] = 'true';
-  }
-  
-  return originalFetch(resource, config);
-};
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
