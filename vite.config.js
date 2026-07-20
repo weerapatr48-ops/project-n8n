d2f9aad7c8ca86659e434706306f4c7a0c8d8872
weerapatr48-ops <weerapat.r48@gmail.com>
2026-07-20 13:29:14 +0700

    Allow ngrok hosts in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/webhook': {
         target: 'http://localhost:5678',
